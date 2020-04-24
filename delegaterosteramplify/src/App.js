aab714bcfa7f955d29053d11a115669daa93597d
Eric Hall <erich@villagecare.org>
2020-04-24 01:14:21 -0400

    trying amplify test 10

--- delegaterosteramplify/src/App.js
+++ delegaterosteramplify/src/App.js
@@ -1,29 +1,26 @@
-import React, { Component } from 'react';
-import {BrowserRouter, Route} from "react-router-dom"
+import React from 'react';
+import logo from './logo.svg';
 import './App.css';
 
-import UploadPage from "./views/upload_delegate_roster"
-
-class App extends Component {
-
-  render(){
-    let routelist=[]
-    
-    routelist=[<Route exact path="/" component={UploadPage}/>]
-
+function App() {
   return (
-    <BrowserRouter>
-      <div>
-        <main>
-         {routelist}
-        </main>
-      </div>
-      </BrowserRouter>
-      
-    
-    
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
-}
 }
 
 export default App;
